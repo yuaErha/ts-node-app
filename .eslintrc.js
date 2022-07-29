32342460ff2d0dbf83fdb043214f8523f3fc2240
yua <2937903263@qq.com>
2022-07-29 23:06:22 +0800

    node-start

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    // 'prettier/@typescript-eslint',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
+    // 'unused-imports/no-unused-imports-ts': 'on',
+    // indent: ['error', 2],
+    // 'linebreak-style': ['error', 'unix'],
+    // quotes: ['error', 'single'],
+    // semi: ['error', 'never'],
+  },
+}
